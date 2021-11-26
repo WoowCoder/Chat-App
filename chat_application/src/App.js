bfd5e9c2c660ab1d2e6788ceac17ae25d916999f
WoowCoder <woow.coder@gmail.com>
2021-11-26 18:25:11 +0300

    Added chat feed component to App.js

--- chat_application/src/App.js
+++ chat_application/src/App.js
@@ -2,13 +2,16 @@ import { ChatEngine } from "react-chat-engine";
 
 import "./App.css";
 
+import ChatFeed from "./components/ChatFeed";
+
 const App = () => {
     return (
         <ChatEngine
             height     = "100vh"
-            projectID  = ""
-            userName   = ""
-            userSecret = ""
+            projectID  = "9d162745-e07c-4e9e-91e3-dd5e76791699"
+            userName   = "Admin"
+            userSecret = "12345"
+            renderChatFeed = {(chatAppProps) => <ChatFeed { ...chatAppProps } /> }
         />
     );
 }

0f08229b933e9f14bab9e665f4c98cf9c1638d10
WoowCoder <woow.coder@gmail.com>
2021-11-28 09:18:46 +0300

    Added functionality to MyMessage

--- chat_application/src/components/MyMessage.jsx
+++ chat_application/src/components/MyMessage.jsx
@@ -1,5 +1,20 @@
-const MyMessage = () => {
-  return <div> MyMessage </div>;
+const MyMessage = ( {message} ) => {
+  
+  if(message?.attachments?.length > 0) {
+    return (
+      <img src       = {message.attachments[0].file} 
+           alt       = "message-attachments"
+           style     = { {float: "right"} }
+           className = "message-image"
+      />
+    )
+  }
+
+  return (
+    <div className = "message" style = { {float: "right", marginRight: "18px", color: "white", backgroundColor: "#3B2A50"} }> 
+      {message.text}
+    </div>
+  );
 };
 
 export default MyMessage;

9c48ac0e7aff28afcfd0bcf5d19802af31e0493d
WoowCoder <woow.coder@gmail.com>
2021-11-28 10:24:22 +0300

    Added functionality to TheirMessage

--- chat_application/src/components/TheirMessage.jsx
+++ chat_application/src/components/TheirMessage.jsx
@@ -1,5 +1,33 @@
-const TheirMessage = () => {
-  return <div> TheirMessage </div>;
+const TheirMessage = ( {lastMessage, message} ) => {
+  const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+  
+  return (
+    <div className = "message-row"> 
+
+      { isFirstMessageByUser && (
+        <div
+          className = "message-avatar"
+          style     = { {backgroundImage: `url( ${message?.sender?.avatar} )`} }
+        />
+      )} 
+
+      {message?.attachments?.length > 0 
+        ? (
+            <img 
+              src        = {message.attachments[0].file} 
+              alt        = "message-attachments"
+              style      = { {marginLeft: isFirstMessageByUser ? "4px" : "48px"} }
+              className  = "message-image"
+            />
+        ) : (
+            <div className = "message" style = { {float: "left", backgroundColor: "#CABCDC", marginLeft: isFirstMessageByUser ? "4px" : "48px"} }> 
+              {message.text}
+            </div>
+        )
+      }
+
+    </div>
+  );
 };
 
 export default TheirMessage;
